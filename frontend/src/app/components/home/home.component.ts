e2c1c06cf16a2b8e9c96f6bc8be18fe138ed1adf
JoaquinFiorio <97994656+JoaquinFiorio@users.noreply.github.com>
2023-07-14 23:49:12 -0300

    update

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -10,7 +10,9 @@ import { ProductoServiceService } from 'src/app/servicios/producto-service.servi
 })
 export class HomeComponent {
 
-  productos: Photo[] = []
+  productos: Photo[] = [];
+  filter = false;
+  filteredProductos: Photo[] = [];
 
   constructor(private producto: ProductoServiceService, private router: Router) {}
 
@@ -30,6 +32,20 @@ export class HomeComponent {
 
   selectedCard(id: string) {
     this.router.navigate(['/producto', id]);
+  }
+
+  mostrarValor(valor: string) {
+    if(valor === 'Todos los productos') {
+      this.filter = false;
+    } else {
+      this.filter = true;
+      this.filteredProductos = this.productos.filter(p => p.categoria === valor )
+    }
+  }
+
+  submitForm(formValue: any) {
+    this.filter = true;
+    this.filteredProductos = this.productos.filter(p => p.title.includes(formValue.producto))
   }
 
 }
